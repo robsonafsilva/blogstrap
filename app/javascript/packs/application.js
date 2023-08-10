8fe46d4e051a9b9f96469d60fe70269f7bf72875
robsonafsilva <robsonap.silva@gmail.com>
2023-08-10 15:44:26 -0300

    Initial Commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,23 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+require('@popperjs/core')
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bootstrap'
+import { Tooltip } from 'bootstrap'
+import '../stylesheets/application'
+
+document.addEventListener("turbolinks:load", () => {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
+
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
